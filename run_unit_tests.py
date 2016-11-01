071e6b6103e04e3e28dc8e9d00fa0191a0d91fe5
Joe Masters <jmasters@nrao.edu>
2016-11-01 09:48:17 -0400

    Changed run_unit_tests from bash script to python.

---
+++ run_unit_tests.py
@@ -0,0 +1,16 @@
+import os
+
+casapath = '/users/jmasters/lustre/casa-unittests'
+os.environ['CASAPATH'] = casapath
+os.environ['PATH'] = '{casapath}:{casapath}/bin:{path}'.format(casapath=casapath, path=os.environ['PATH'])
+
+print('CASAPATH = {}'.format(casapath))
+
+
+def run_all_tests():
+    os.system('time $CASAPATH/bin/casa --nogui --nologger -c '
+              '$CASAPATH/lib/python2.7/regressions/admin/runUnitTest.py '
+              '--datadir ~/lustre/unittests/data/ --file ~/lustre/unittests/tests/unittests_list.txt')
+
+if __name__ == '__main__':
+    run_all_tests()
