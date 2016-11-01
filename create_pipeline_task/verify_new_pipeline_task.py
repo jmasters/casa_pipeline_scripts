9b5c76ae34e64f9e59064b98ec8dd95d0f7b4dd6
Joe Masters <jmasters@nrao.edu>
2016-11-01 14:10:58 -0400

    Added support mako templates to create_pipeline_task.
    I also put all these files in a single package, also
    called create_pipeline_task.

---
+++ create_pipeline_task/verify_new_pipeline_task.py
@@ -0,0 +1,33 @@
+import pipeline
+
+with open(os.environ["SCIPIPE_HEURISTICS"] + "/pipeline/h/cli/h.py") as fd:
+    exec (fd.read())
+
+with open(os.environ["SCIPIPE_HEURISTICS"] + "/pipeline/hif/cli/hif.py") as fd:
+    exec (fd.read())
+
+with open(os.environ["SCIPIPE_HEURISTICS"] + "/pipeline/hifa/cli/hifa.py") as fd:
+    exec (fd.read())
+
+with open(os.environ["SCIPIPE_HEURISTICS"] + "/pipeline/hifv/cli/hifv.py") as fd:
+    exec (fd.read())
+
+vis = '/lustre/naasc/users/jmasters/pipeline_test_data/VLAT003/rawdata/13A-537.sb24066356.eb24324502.56514.05971091435'
+
+h_init()
+h_save()
+hifv_importdata(vis=[vis], session=['session_1'], overwrite=False)
+h_save()
+
+context = pipeline.Pipeline(context='last').context
+
+print('checking pipeline.hif.tasks.foobar.Foobar(inputs).execute()')
+inputs = pipeline.hif.tasks.foobar.Foobar.Inputs(context)
+task = pipeline.hif.tasks.foobar.Foobar(inputs)
+result = task.execute(dry_run=False)
+result.accept(context)
+context.save()
+
+print('checking hif_foobar()')
+hif_foobar()
+h_save()
