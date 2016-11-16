fd89b8657648d10b5c419e32926098c516a5850f
Joe Masters <jmasters@nrao.edu>
2016-11-16 12:41:05 -0500

    Made verify_new_pipeline_task.py generic.

--- new_pipeline_task/verify_new_pipeline_task.py
+++ new_pipeline_task/verify_new_pipeline_task.py
@@ -1,60 +1,79 @@
-import pipeline
+"""
+Verify a new CASA pipeline task.
+
+The purpose of this check a newly created pipeline task.
+"""
+
+import os
+import sys
 import argparse
 
+import pipeline
 
-def parse_command_line(argv):
-    script_args = ''
-    for idx, arg in enumerate(argv):
-        if 'verify_new_pipeline_task.py' in arg:
-            script_args = argv[idx + 1:]
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--package', help="Pipeline package.  One of 'h', 'hif', 'hifa', 'hifa', 'hifv', or 'hsd'.",
-                        type=str, choices=['h', 'hif', 'hifa', 'hifa', 'hifv', 'hsd'], required=True)
-    parser.add_argument('--task', help='New task name', type=str, required=True)
+class NewTask():
 
-    try:
-        args = parser.parse_args(script_args)
-    except:
-        print('Problem with new_pipeline_task arguments.')
-        return '', ''
+    def __init__(self):
+        self.repository_path = os.environ['SCIPIPE_HEURISTICS']
 
-    area = args.package
-    task_name = args.task
-    return area, task_name
+    def parse_command_line(self, argv):
+        script_args = ''
+        for idx, arg in enumerate(argv):
+            if 'verify_new_pipeline_task.py' in arg:
+                script_args = argv[idx + 1:]
 
-area, task_name = parse_command_line(sys.argv)
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--package', help="Pipeline package.  One of 'h', 'hif', 'hifa', 'hifa', 'hifv', or 'hsd'.",
+                            type=str, choices=['h', 'hif', 'hifa', 'hifa', 'hifv', 'hsd'], required=True)
+        parser.add_argument('--task', help='New task name', type=str, required=True)
+        parser.add_argument('--test-ms', dest='vis', help='Test Measurement Set', type=str, required=True)
 
-# ----------------------------------------------
-#   set data, package and task name for testing
-# ----------------------------------------------
-vis = '/lustre/naasc/users/jmasters/unittests/data/utils/small.ms'
+        try:
+            args = parser.parse_args(script_args)
+        except:
+            print('Problem with new_pipeline_task arguments.')
+            return '', '', ''
 
-# ---------------------------
-#  prepare to check the task
-# ---------------------------
-h_init()
-h_save()
-hifv_importdata(vis=[vis], session=['session_1'], overwrite=False)
-h_save()
+        area = args.package
+        task_name = args.task
+        vis = args.vis
+        return area, task_name, vis
 
-context = pipeline.Pipeline(context='last').context
+    def verify(self, area, task_name, vis):
+        # ---------------------------
+        #  prepare to check the task
+        # ---------------------------
+        h_init()
+        h_save()
+        hifv_importdata(vis=[vis], session=['session_1'], overwrite=False)
+        h_save()
 
-# ---------------------------
-#  execute the task
-# ---------------------------
-inputs = eval('pipeline.{area}.tasks.{task}.{captask}.Inputs(context)'.format(area=area, task=task_name, captask=task_name.capitalize()))
-task = eval('pipeline.{area}.tasks.{task}.{captask}(inputs)'.format(area=area, task=task_name, captask=task_name.capitalize()))
-result = task.execute(dry_run=False)
-result.accept(context)
-context.save()
+        context = pipeline.Pipeline(context='last').context
 
-# --------------------------------
-#   run as a registered CASA task
-# --------------------------------
-try:
-    eval('{area}_{task}()'.format(area=area, task=task_name))
-    h_save()
-except NameError as ee:
-    print('ERROR: {msg}'.format(msg=ee.message))
-    print('\tTry using runsetup to register the new task with CASA first.')
+        # ---------------------------
+        #  execute the task
+        # ---------------------------
+        inputs = eval('pipeline.{area}.tasks.{task}.{captask}.Inputs(context)'.format(area=area, task=task_name, captask=task_name.capitalize()))
+        task = eval('pipeline.{area}.tasks.{task}.{captask}(inputs)'.format(area=area, task=task_name, captask=task_name.capitalize()))
+        result = task.execute(dry_run=False)
+        result.accept(context)
+        context.save()
+
+        # --------------------------------
+        #   run as a registered CASA task
+        # --------------------------------
+        try:
+            eval('{area}_{task}()'.format(area=area, task=task_name))
+            h_save()
+        except NameError as ee:
+            print('ERROR: {msg}'.format(msg=ee.message))
+            print('\tTry using runsetup to register the new task with CASA first.')
+
+if __name__ == '__main__':
+
+    new_task = NewTask()
+    area, task_name, vis = new_task.parse_command_line(sys.argv)
+    if not area or not task_name or not vis:
+        print('area, task_name and test-ms all need to be defined')
+    else:
+        new_task.verify(area, task_name, vis)
